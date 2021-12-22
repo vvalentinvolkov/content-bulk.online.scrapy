f3ad04fa8bafb9caa20c09841f246ba8eea3a1bb
bulkinegra_local <v.valentinvolkov@gmail.com>
2021-12-22 18:22:23 +0300

    Конфиг гуникора и wsgi вмсето api - app

---
+++ gunicorn.config.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+command = '/home/bilkinegra/my_project/content-bulk.online.scrapy/.venv/bin/gunicorn'
+# pythonpath = '/home/www/...' # Абсл. пути для sys.path, через запятую
+bind = '127.0.0.1:8001'
+workers = multiprocessing.cpu_count() * 2 + 1
+limit_request_fields = 32000 # Ограничение колличества полей в HTTP head
+limit_request_field_size = 0 # Ограничение размера HTTP head (0 - не огран.)
+raw_env = ["TESTING=True"] # Переменные среды для запуска
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from src.rest_api import app as application
+
+app = application.app
+
+if __name__ == '__main__':
+    app.run(debug=True)
