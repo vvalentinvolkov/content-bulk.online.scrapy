e4d296865978fde8865e29940aee6551ec5f07af
bulkinegra <v.valentinvolkovwr@gmail.com>
2021-12-27 20:45:16 +0300

    при limit None возвращает максимальное значение элементов а не 1

--- src/rest_api/resources.py
+++ src/rest_api/resources.py
@@ -1,9 +1,11 @@
 import logging
 
+import flask
 from flask_restful import Resource, reqparse,  abort
 
 from src.services import db_services
 from src.services.models import ZenArticle, ZenFeed
+from src.services.query_set_converter import ConvertibleQuerySet
 
 logger = logging.getLogger(__name__)
 
@@ -33,4 +35,4 @@ class BulkResource(Resource):
         else:
             args_ = self.parser.parse_args()
             query_set = db_services.get_query_set(document=model_class, **args_)
-            return query_set.to_json(ensure_ascii=False), 200
+            return query_set.to_json(), 200
